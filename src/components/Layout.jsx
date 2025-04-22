3ce12e37c4d38cffd47000fa975e4f765915d1c0
Aayush Thorat <aayush.thorat27@gmail.com>
2025-04-22 21:45:56 +0530

    Update Layout.jsx

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Footer from './Footer'
 import { Outlet } from 'react-router-dom'
-import Navbar from './NavBar'
+import Navbar from './Navbar'
 // import RSB from './Booking_button'
 export default function Layout() {
   return (
